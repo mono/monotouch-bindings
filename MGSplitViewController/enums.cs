3db1774c47abf03c4b180fd7c4b66f89272cb525
Miguel de Icaza <miguel@gnome.org>
2010-10-02 22:14:16 -0400

    Add bindings to MGSplitViewController

---
+++ MGSplitViewController/enums.cs
@@ -0,0 +1,11 @@
+
+namespace MG {
+	public enum MGSplitViewDividerStyle {
+		Thin,
+		PaneSplitter
+	}
+
+	public enum MGCornersPosition {
+		LeadingVertical, TrailingVertical, LeadingHorizontal, TrailingHorizontal
+	}
+}
