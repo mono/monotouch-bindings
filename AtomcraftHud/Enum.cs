287330e6fee29694d34c78dfc7b40dcde593335a
Chris Hardy <chrisntr@gmail.com>
2011-01-03 21:26:52 +0000

    Added in Atomcraft HUD binding - http://www.atomcraft.de/blog/2010/12/23/atomhud-release/
    Signed-off-by: Chris Hardy <chrisntr@gmail.com>

---
+++ AtomcraftHud/Enum.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Atomcraft
+{
+	public enum ATMHudAccessoryPosition {
+		ATMHudAccessoryPositionTop,
+		ATMHudAccessoryPositionRight,
+		ATMHudAccessoryPositionBottom,
+		ATMHudAccessoryPositionLeft
+	}
+}
+
