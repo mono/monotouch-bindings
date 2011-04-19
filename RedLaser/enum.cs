1dca2654fa423842210af784142735bac18ea98f
Chris Branson <branson.chris@gmail.com>
2011-04-19 11:33:14 +0100

    Updated to RedLaser SDK 3.0.0

--- RedLaser/enum.cs
+++ RedLaser/enum.cs
@@ -1,7 +1,11 @@
 /*
  * Updated 24/8/2010, Chris Branson, updated to RedLaser SDK 2.8.2
  * 
- * Updated 16/2/2010, Chris Branson, updated to RedLaser SDK 2.9.2
+ * Updated 16/2/2011, Chris Branson, updated to RedLaser SDK 2.9.2
+ *
+ * Updated 19/4/2011, Chris Branson, updated to RedLaser SDK 3.0.0
+ * 
+ * This is the public API for the RedLaser SDK.
  *
 */
 
@@ -14,5 +18,7 @@ public enum RLBarcodeType {
 	Code128 = 0x20,
 	Code39 = 0x40,
 	DataMatrix = 0x80,
-	ITF = 0x100
+	ITF = 0x100,
+	EAN5 = 0x200,
+	EAN2 = 0x400
 }
