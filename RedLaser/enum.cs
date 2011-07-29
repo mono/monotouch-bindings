1957f03965808f47f12731f83d1075b70901c9f1
Chris Branson <branson.chris@gmail.com>
2011-07-29 14:38:45 +0100

    Updated to RedLaser SDK 3.1.1

--- RedLaser/enum.cs
+++ RedLaser/enum.cs
@@ -4,11 +4,15 @@
  * Updated 16/2/2011, Chris Branson, updated to RedLaser SDK 2.9.2
  *
  * Updated 19/4/2011, Chris Branson, updated to RedLaser SDK 3.0.0
+ *
+ * Updated 29/7/2011, Chris Branson, updated to RedLaser SDK 3.1.1
  * 
  * This is the public API for the RedLaser SDK.
  *
 */
 
+namespace RedLaser
+{
 public enum RLBarcodeType {
 	EAN13 = 0x1,
 	UPCE = 0x2,
@@ -21,4 +25,15 @@ public enum RLBarcodeType {
 	ITF = 0x100,
 	EAN5 = 0x200,
 	EAN2 = 0x400
+}
+
+public enum RedLaserStatus {
+	RLState_EvalModeReady = 1,
+	RLState_LicensedModeReady = 2,
+	
+	RLState_MissingOSLibraries = -1,
+	RLState_NoCamera = -2,
+	RLState_BadLicense = -3,
+	RLState_ScanLimitReached = -4,
+}
 }
