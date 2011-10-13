842fb83cfdd14619523324da1881f8379a02a6fe
Jeffrey Stedfast <jeff@xamarin.com>
2011-10-13 18:46:40 -0400

    [Three20] Redid the Three20 bindings to take advantage of LinkWith

--- Three20/binding/enums.cs
+++ Three20/binding/enums.cs
@@ -1,8 +1,10 @@
+using System;
 
+namespace Three20 {
 	public enum TTActivityLabelStyle { TTActivityLabelStyle1 = 1 }
 	public enum TTURLRequestCachePolicy { TTURLRequestCachePolicy1 = 1 }
 	public enum TTPosition { TTPosition1 = 1 }
 	public enum TTNavigationMode { TTNavigationMode1 = 1 }
 	public enum TTNavigatorPersistenceMode { TTNavigatorPersistenceMode1 = 1 }
 	public enum TTPhotoVersion { TTPhotoVersion1 = 1 }
-
+}
