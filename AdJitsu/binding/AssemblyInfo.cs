775ab13670e8cd9b4c5153400d9ac0b3d801d335
Jeffrey Stedfast <jeff@xamarin.com>
2011-10-25 14:31:53 -0400

    [AdJitsu] Specify the frameworks needed by the library.

--- AdJitsu/binding/AssemblyInfo.cs
+++ AdJitsu/binding/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libAdJitsuSDK.a", LinkTarget.Simulator | LinkTarget.ArmV7, "-lxml2 -lsqlite3", ForceLoad = true, IsCxx = true)]
+[assembly: LinkWith ("libAdJitsuSDK.a", LinkTarget.Simulator | LinkTarget.ArmV7, "-lxml2 -lsqlite3", Frameworks = "AVFoundation CoreGraphics CoreMedia CoreMotion CoreText MediaPlayer OpenGLES QuartzCore", ForceLoad = true, IsCxx = true)]
