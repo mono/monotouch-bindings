d694d63ea7331a75ee42836f2400546dd6f95ae1
Jeffrey Stedfast <jeff@xamarin.com>
2011-10-25 15:04:12 -0400

    [CorePlot] Added required Frameworks

--- CorePlot/binding/AssemblyInfo.cs
+++ CorePlot/binding/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libCorePlot-CocoaTouch.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true)]
+[assembly: LinkWith ("libCorePlot-CocoaTouch.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, Frameworks = "CoreGraphics QuartzCore", ForceLoad = true)]
