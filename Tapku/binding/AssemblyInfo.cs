c2ac6078401f566f550d510c37508011fa7faebb
Jeffrey Stedfast <jeff@xamarin.com>
2011-10-31 17:20:57 -0400

    [Tapku] Added QuartzCore to list of required Frameworks

--- Tapku/binding/AssemblyInfo.cs
+++ Tapku/binding/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libTapkuSDK.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true)]
+[assembly: LinkWith ("libTapkuSDK.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, Frameworks = "QuartzCore", ForceLoad = true)]
