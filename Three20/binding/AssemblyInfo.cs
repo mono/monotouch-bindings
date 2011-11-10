68871897ebf11d2a69853bf3b369b3f15e376215
Jeffrey Stedfast <jeff@xamarin.com>
2011-11-10 11:54:03 -0500

    [Three20] Added QuartzCore to the LinkWIth for libThree20UISDK.a

--- Three20/binding/AssemblyInfo.cs
+++ Three20/binding/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using MonoTouch.ObjCRuntime;
 [assembly: LinkWith ("libThree20CoreSDK.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true)]
 [assembly: LinkWith ("libThree20NetworkSDK.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true)]
 [assembly: LinkWith ("libThree20StyleSDK.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true)]
-[assembly: LinkWith ("libThree20UISDK.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true)]
+[assembly: LinkWith ("libThree20UISDK.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, Frameworks = "QuartzCore", ForceLoad = true)]
 [assembly: LinkWith ("libThree20UICommonSDK.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true)]
 [assembly: LinkWith ("libThree20UINavigatorSDK.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true)]
