9bd098254701664a10211fb5ca7bf0c373305132
Miguel de Icaza <miguel@gnome.org>
2011-11-28 15:11:52 -0500

    Workaround Google's Broken NoThumb.a library
    We provide a stub class that contains the missing methods and we add this
    to the library that Google shipped and embed the result in our assembly

--- NotYetGoogleAnalytics/binding/AssemblyInfo.cs
+++ NotYetGoogleAnalytics/binding/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libGoogleAnalytics_NoThumb.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, "-lsqlite3.0", Frameworks = "CFNetwork", ForceLoad = true)]
+[assembly: LinkWith ("libFixedGoogleAnalytics.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, "-lsqlite3.0", Frameworks = "CFNetwork", ForceLoad = true)]
---
+++ NotYetGoogleAnalytics/binding/stub.m
@@ -0,0 +1,14 @@
+/*
+ * Empty stub for Google's Broken distribution of their
+ * _NoThumb.a library
+ */
+#import <Foundation/Foundation.h>
+
+@interface GANAdHitIdGenerator : NSObject {
+}
+
+@end
+
+@implementation GANAdHitIdGenerator 
+
+@end
