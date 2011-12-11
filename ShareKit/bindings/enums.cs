4cb1031890afa2ceaa1ff8b56cd7f7c64b15d4af
Miguel de Icaza <miguel@gnome.org>
2011-12-11 12:11:02 -0500

    Added ShareKit

---
+++ ShareKit/bindings/enums.cs
@@ -0,0 +1,13 @@
+namespace MonoTouch.ShareKit {
+	public enum ShkShareType {
+		Undefined, Url, Text, Image, File
+	}
+
+	public enum ShkFormFieldType {
+		Text, Password, Switch
+	}
+
+	public enum ShkSharerPendingAction {
+		None, Share, RefreshToken
+	}
+}
