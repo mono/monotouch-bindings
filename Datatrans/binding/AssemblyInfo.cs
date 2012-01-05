9691b3bcaa929ed902225db7538f6176e32b1e0a
lduchosal <456t85z7g>
2012-01-05 19:20:42 +0100

    Including resources as embeded

--- Datatrans/binding/AssemblyInfo.cs
+++ Datatrans/binding/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libdtipl.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true)]
+[assembly: LinkWith ("libdtipl.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true, LinkerFlags = "-lxml2")]
