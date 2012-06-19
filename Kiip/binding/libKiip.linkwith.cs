6a0be7bfa3d3fce224934dc2f5df34bfd8701641
Clancey <james.clancey@gmail.com>
2012-06-19 10:40:08 -0800

    Updated the parse sample to be arm6+7.  Fixed the kiip binding.

--- Kiip/binding/libKiip.linkwith.cs
+++ Kiip/binding/libKiip.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libKiip.a",   LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Simulator,Frameworks = "MobileCoreServices CoreGraphics CFNetwork SystemConfiguration", ForceLoad = true)]
+[assembly: LinkWith ("libKiip.a", LinkTarget.ArmV7 | LinkTarget.Simulator,Frameworks = "MobileCoreServices CoreGraphics CFNetwork SystemConfiguration", LinkerFlags="-lxml2", ForceLoad = true)]
