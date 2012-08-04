ad798d2296bee5a788ae55a5c016ec5a41190fa6
Miguel de Icaza <miguel@gnome.org>
2012-08-04 15:39:14 -0400

    Bind a lot of the proxies

--- Parse/binding/extra.cs
+++ Parse/binding/extra.cs
@@ -87,5 +87,179 @@ namespace ParseLib {
 			FetchAllIfNeededAsync (objects, d, NSActionDispatcher.Selector);
 		}
 	}
+	public partial class ParsePush {
+		public void SendPushMessageAsync (string channel, string message, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			SendPushMessageAsync (channel, message, d, NSActionDispatcher.Selector);
+		}
+		
+		public void SendPushAsync (NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			SendPushAsync (d, NSActionDispatcher.Selector);			
+		}
+		
+		public void SendPushDataAsync (string channel, NSDictionary data, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			SendPushDataAsync (channel, data, d, NSActionDispatcher.Selector);			
+		}
+		
+		public void GetSubscribedChannelsAsync (NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			GetSubscribedChannelsAsync (d, NSActionDispatcher.Selector);			
+		}
+		
+		public void SubscribeToChannelAsync (string channel, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			SubscribeToChannelAsync (channel, d, NSActionDispatcher.Selector);
+		}
+		
+		public void UnsubscribeFromChannelAsync (string channel, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			UnsubscribeFromChannelAsync (channel, d, NSActionDispatcher.Selector);			
+		}
+	}
+	
+	public partial class ParseQuery {
+		public void GetObjectAsync (string objectId, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			GetObjectAsync (objectId, d, NSActionDispatcher.Selector);				
+		}
+		
+		public void FindObjectsAsync (NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			FindObjectsAsync (d, NSActionDispatcher.Selector);				
+		}
+			
+		public void GetFirstObjectAsync (NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			GetFirstObjectAsync (d, NSActionDispatcher.Selector);				
+		}
+			
+		public void CountObjectsAsync (NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			CountObjectsAsync (d, NSActionDispatcher.Selector);				
+		}
+	}
+	
+	public partial class ParseUser {
+		public void SignUpAsync (NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			SignUpAsync (d, NSActionDispatcher.Selector);				
+		}
+		
+		public void LogInWithAsync (string username, string password, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			LogInWithAsync (username, password, d, NSActionDispatcher.Selector);				
+		}
+		
+		public void RequestPasswordResetAsync (string email, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			RequestPasswordResetAsync (email, d, NSActionDispatcher.Selector);				
+		}
+	}
+		
+	partial class ParseAnonymousUtils {
+		public void LogInWithTargetselector (NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			LogInWithTargetselector (d, NSActionDispatcher.Selector);			
+		}
+	}
+	
+	partial class ParseMBProgressHUD {
+		public void ShowWhileExecuting (NSAction method, NSAction callback, bool animated)
+		{
+			// Read the docs.
+		}
+	}
+
+	partial class ParseFacebookUtils {
+		public void LogInAsync (NSArray permissions, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			LogInAsync (permissions, d, NSActionDispatcher.Selector);
+		}
+		
+		public void LogInAsync (string facebookId, string accessToken, NSDate expirationDate, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			LogInAsync (facebookId, accessToken, expirationDate, d, NSActionDispatcher.Selector);			
+		}
+		
+		public void LinkUserAsync (ParseUser user, NSArray permissions, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			LinkUserAsync (user, permissions, d, NSActionDispatcher.Selector);			
+		}
+		
+		public void LinkUserAsync (ParseUser user, string facebookId, string accessToken, NSDate expirationDate, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			LinkUserAsync (user, facebookId, accessToken, expirationDate, d, NSActionDispatcher.Selector);			
+		}
+		
+		public void UnlinkUserAsync (ParseUser user, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			UnlinkUserAsync (user, d, NSActionDispatcher.Selector);			
+		}
+		
+		public void ExtendAccessTokenForUser (ParseUser user, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			ExtendAccessTokenForUser (user, d, NSActionDispatcher.Selector);			
+		}
+		
+		public bool ExtendAccessTokenIfNeeded (ParseUser user, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			return ExtendAccessTokenIfNeeded (user, d, NSActionDispatcher.Selector);			
+		}
+	}
+	
+	partial class ParseTwitterUtils {
+		public void LogIn (NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			LogIn (d, NSActionDispatcher.Selector);			
+		}
+		
+		public void LogInWithTwitterId (string twitterId, string screenName, string authToken, string authTokenSecret, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			LogInWithTwitterId (twitterId, screenName, authToken, authTokenSecret, d, NSActionDispatcher.Selector);			
+		}
+		
+		public void LinkUserAsync (ParseUser user, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			LinkUserAsync (user, d, NSActionDispatcher.Selector);			
+		}
+		
+		public void LinkUserAsync (ParseUser user, string twitterId, string screenName, string authToken, string authTokenSecret, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			LinkUserAsync (user, twitterId, screenName, authToken, authTokenSecret, d, NSActionDispatcher.Selector);			
+		}
+		
+		public void UnlinkUserAsync (ParseUser user, NSAction callback)
+		{
+			var d = new NSActionDispatcher (callback);
+			UnlinkUserAsync (user, d, NSActionDispatcher.Selector);			
+		}
+	}
 	
 }
