492f68c3c2007f0638452cc8a5a762556db224ba
Miguel de Icaza <miguel@gnome.org>
2012-08-07 21:50:56 -0400

    Fix

--- Couchbase/binding/libCouchCocoa.linkwith.cs
+++ Couchbase/binding/libCouchCocoa.linkwith.cs
@@ -2,3 +2,4 @@ using System;
 using MonoTouch.ObjCRuntime;
 
 [assembly: LinkWith ("libCouchCocoa.a", LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true)]
+[assembly: LinkWith ("libTouchDB.a", LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true)]
