63a831eeae9f9c990959a28bd260316c6b0c488f
Jeffrey Stedfast <jeff@xamarin.com>
2012-11-13 15:07:20 -0500

    [Facebook] libFacebook.a seems to require libsqlite3

--- facebook/binding/AssemblyInfo.cs
+++ facebook/binding/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libFacebookSDK.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, ForceLoad = true, Frameworks = "CoreGraphics")]
+[assembly: LinkWith ("libFacebookSDK.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, "sqlite3", ForceLoad = true, Frameworks = "CoreGraphics")]
