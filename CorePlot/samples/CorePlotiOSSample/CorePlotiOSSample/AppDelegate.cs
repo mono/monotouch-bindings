b4118e1430fef65963a9cac3532b71dd0adc4fb5
David Siegel <djsiegel@gmail.com>
2013-01-11 13:52:19 -0800

    Remove pedantic iOS comments that don't pertain to Core Plot

--- CorePlot/samples/CorePlotiOSSample/CorePlotiOSSample/AppDelegate.cs
+++ CorePlot/samples/CorePlotiOSSample/CorePlotiOSSample/AppDelegate.cs
@@ -33,9 +33,6 @@ using MonoTouch.UIKit;
 
 namespace CorePlotiOSSample
 {
-	// The UIApplicationDelegate for the application. This class is responsible for launching the 
-	// User Interface of the application, as well as listening (and optionally responding) to 
-	// application events from iOS.
 	[Register ("AppDelegate")]
 	public partial class AppDelegate : UIApplicationDelegate
 	{
@@ -43,19 +40,10 @@ namespace CorePlotiOSSample
 		DialogViewController dvc;
 		UIWindow window;
 
-		//
-		// This method is invoked when the application has loaded and is ready to run. In this 
-		// method you should instantiate the window, load the UI into it and then make the window
-		// visible.
-		//
-		// You have 17 seconds to return from this method, or iOS will terminate your application.
-		//
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
 			window = new UIWindow (UIScreen.MainScreen.Bounds);
-			
-			// If you have defined a view, add it here:
-			// window.AddSubview (navigationController.View);
+
 			var root = new RootElement ("CorePlot Samples"){
 				new Section () {
 					new RootElement ("XYGraph", r => new ScatterPlot ()),
