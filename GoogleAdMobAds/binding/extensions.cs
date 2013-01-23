3932959f797ef40ad2da1f86b1614a1d063b6d47
Alex Soto <dxdream@gmail.com>
2013-01-22 18:00:40 -0600

    Updated GoogleAdmobAds to version 6.2.1

--- GoogleAdMobAds/binding/extensions.cs
+++ GoogleAdMobAds/binding/extensions.cs
@@ -1,28 +1,61 @@
 using System;
+using System.Runtime.InteropServices;
+using MonoTouch.ObjCRuntime;
 using MonoTouch.Foundation;
 using System.Drawing;
 
 namespace GoogleAdMobAds
 {
-	partial class GADBannerView
-	{
-		// iPhone and iPod Touch ad size.
-		public static readonly SizeF GAD_SIZE_320x50 = new SizeF(320, 50);
-
-		// Medium Rectangle size for the iPad (especially in a UISplitView's left pane).
-		public static readonly SizeF GAD_SIZE_300x250 = new SizeF(300, 250);
-
-		// Full Banner size for the iPad (especially in a UIPopoverController or in
-		// UIModalPresentationFormSheet).
-		public static readonly SizeF GAD_SIZE_468x60 = new SizeF(468, 60);
-
-		// Leaderboard size for the iPad.
-		public static readonly SizeF GAD_SIZE_728x90 = new SizeF(728, 90);	
-	}
-	
-	partial class GADRequest
+	public partial class GADRequest
 	{
 		public static readonly string GAD_SIMULATOR_ID = "Simulator";
+		
+		public static readonly string GADGoogleAdMobNetworkName = "GoogleAdMobAds";
+	}
+	
+	public partial class GADRequestError
+	{
+		private static string kGADErrorDomain;
+		
+		public static string ErrorDomain
+		{
+			get 
+			{
+				IntPtr RTLD_MAIN_ONLY = Dlfcn.dlopen (null, 0);
+				kGADErrorDomain = (string) Dlfcn.GetStringConstant (RTLD_MAIN_ONLY, "kGADErrorDomain");
+				
+				return kGADErrorDomain;
+			}
+			set 
+			{
+				kGADErrorDomain = value;
+				
+				IntPtr RTLD_MAIN_ONLY = Dlfcn.dlopen (null, 0);
+				IntPtr ptr = Dlfcn.dlsym (RTLD_MAIN_ONLY, "kGADErrorDomain");
+				
+				Marshal.WriteIntPtr(ptr, new NSString(kGADErrorDomain).Handle);
+				
+			}
+		}
+	}
+	
+	public partial class GADAdSizeCons : NSObject
+	{	
+		// Deprecated Macros
+		[Obsolete("Use GADAdSizeCons.Banner Instead")]
+		public static readonly SizeF GAD_SIZE_320x50 = GADAdSizeCons.Banner.size;
+		
+		[Obsolete("Use GADAdSizeCons.MediumRectangle Instead")]
+		public static readonly SizeF GAD_SIZE_300x250 = GADAdSizeCons.MediumRectangle.size;
+		
+		[Obsolete("Use ADAdSizeCons.FullBanner Instead")]
+		public static readonly SizeF GAD_SIZE_468x60 = GADAdSizeCons.FullBanner.size;
+		
+		[Obsolete("Use GADAdSizeCons.Leaderboard Instead")]
+		public static readonly SizeF GAD_SIZE_728x90 = GADAdSizeCons.Leaderboard.size;
+		
+		[Obsolete("Use ADAdSizeCons.Skyscraper Instead")]
+		public static readonly SizeF GAD_SIZE_120x600 = GADAdSizeCons.Skyscraper.size;
 	}
 }
 
