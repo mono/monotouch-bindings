f3a52cb89eacd28182a1f1d02143ee327c2a4a61
Alex Soto <alex@alexsoto.me>
2013-02-02 09:54:45 -0600

    Added support for armv6 on MBProgressHUD

--- MBProgressHUD/binding/AssemblyInfo.cs
+++ MBProgressHUD/binding/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libMBProgressHUD.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, Frameworks = "CoreGraphics")]
+[assembly: LinkWith ("libMBProgressHUD.a", LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, Frameworks = "CoreGraphics")]
