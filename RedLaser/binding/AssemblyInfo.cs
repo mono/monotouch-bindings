2deec96726c7125e73a8f09d80d2618745ad0dbf
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-02-18 15:33:23 +0100

    [RedLaser] Make the QR recognition not crash.

--- RedLaser/binding/AssemblyInfo.cs
+++ RedLaser/binding/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libRedLaserSDK.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, Frameworks = "AudioToolbox AVFoundation CFNetwork CoreMedia CoreVideo OpenGLES Security SystemConfiguration CoreLocation", ForceLoad = true, IsCxx = true, NeedsGccExceptionHandling = true)]
+[assembly: LinkWith ("libRedLaserSDK.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, Frameworks = "AudioToolbox AVFoundation CFNetwork CoreMedia CoreVideo OpenGLES Security SystemConfiguration CoreLocation", ForceLoad = true, IsCxx = true, LinkerFlags = "-lstdc++")]
