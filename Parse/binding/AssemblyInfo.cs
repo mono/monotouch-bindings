733e549e69d4fd206669724cbd48d90ca39ad8e4
Clancey <james.clancey@gmail.com>
2013-02-18 21:38:15 -0800

    Changed parse to use weak frameworks for ios5 compatibility

--- Parse/binding/AssemblyInfo.cs
+++ Parse/binding/AssemblyInfo.cs
@@ -1,4 +1,3 @@
 using System;
 using MonoTouch.ObjCRuntime;
-
-[assembly: LinkWith ("Parse", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, LinkerFlags = "-lsqlite3 -lz",Frameworks = "MobileCoreServices CoreGraphics SystemConfiguration AudioToolbox Security StoreKit AssetsLibrary Accounts AdSupport Social CFNetwork CoreLocation QuartzCore", ForceLoad = true)]
+[assembly: LinkWith ("Parse", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, LinkerFlags = "-lsqlite3 -lz",Frameworks = "MobileCoreServices CoreGraphics SystemConfiguration AudioToolbox Security StoreKit AssetsLibrary CFNetwork CoreLocation QuartzCore", WeakFrameworks="AdSupport Social Accounts", ForceLoad = true)]
