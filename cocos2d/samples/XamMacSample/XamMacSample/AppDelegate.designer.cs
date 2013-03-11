e9ad0b556fc8b7ae3c210a80909eb4af91dad3b6
Stephane Delcroix <stephane@delcroix.org>
2013-03-11 14:16:42 +0100

    [cocos2d] Xam.Mac: fix the sample

--- cocos2d/samples/XamMacSample/XamMacSample/AppDelegate.designer.cs
+++ cocos2d/samples/XamMacSample/XamMacSample/AppDelegate.designer.cs
@@ -1,10 +1,25 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoMac.Foundation;
 
 namespace XamMacSample
 {
-	// Should subclass MonoMac.AppKit.NSResponder
-	[MonoMac.Foundation.Register("AppDelegate")]
-	public partial class AppDelegate
+	[Register ("AppDelegate")]
+	partial class AppDelegate
 	{
+		[Outlet]
+		MonoMac.Cocos2D.CCGLView glView { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (glView != null) {
+				glView.Dispose ();
+				glView = null;
+			}
+		}
 	}
 }
-
