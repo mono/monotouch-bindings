b3eab1c26245d2cf3c9f93aa1ce95b75b6612c76
Alex Soto <alex@alexsoto.me>
2013-03-19 17:27:02 -0600

    [facebook] Files Cleanup

--- facebook/binding/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using MonoTouch.ObjCRuntime;
-
-[assembly: LinkWith ("libFacebookSDK.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, "-ObjC -lsqlite3", ForceLoad = true, Frameworks = "CoreGraphics CoreLocation QuartzCore", WeakFrameworks = "Accounts AdSupport Social")]
