7d343654ad866568bfb7823985f0c7b807123e13
Stephan van Rooij <stephan@svrooij.nl>
2013-03-23 17:58:04 +0100

    [TestFlight] Update to 1.2.4 and settings included
    This commit updates the TestFlight SDK to 1.2.4 and exposes the
    TestFlight Settings as C# methodes.

--- TestFlight/binding/testflight-threadsafe.cs
+++ TestFlight/binding/testflight-threadsafe.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Runtime.InteropServices;
+using MonoTouch.Foundation;
 
 namespace MonoTouch.TestFlight
 {
-	public partial class TestFlight
+	public partial class TestFlight : NSObject
 	{
-		public static void TakeOffThreadSafe(String appToken)
+		public static void TakeOffThreadSafe(String applicationToken)
 		{
 
 				IntPtr sigbus = Marshal.AllocHGlobal (512);
@@ -18,7 +19,7 @@ namespace MonoTouch.TestFlight
 				// Enable crash reporting libraries
 				//MonoTouch.TestFlight.TestFlight.SetDeviceIdentifier(UIDevice.CurrentDevice.UniqueIdentifier);
 				//MonoTouch.TestFlight.TestFlight.SetDeviceIdentifier(MonoTouch.AdSupport.ASIdentifierManager.SharedManager.AdvertisingIdentifier.ToString());
-				TestFlight.TakeOff(appToken);
+				TestFlight.TakeOff(applicationToken);
 				
 				// Restore Mono SIGSEGV and SIGBUS handlers            
 				sigaction (Signal.SIGBUS, sigbus, IntPtr.Zero);
@@ -28,6 +29,43 @@ namespace MonoTouch.TestFlight
 				Marshal.FreeHGlobal (sigsegv);
 
 		}
+
+		private static void setOption(NSString option, Boolean newValue)
+		{
+			TestFlight.SetOptions(new NSDictionary(option,NSNumber.FromBoolean(newValue)));
+		}
+
+		public static void setAttachBacktraceToFeedback(Boolean newValue)
+		{
+			setOption(new NSString("attachBacktraceToFeedback"),newValue);
+		}
+
+		public static void setDisableInAppUpdates(Boolean newValue)
+		{
+			setOption(new NSString("disableInAppUpdates"),newValue);
+		}
+
+		public static void setLogToConsole(Boolean newValue)
+		{
+			setOption(new NSString("logToConsole"),newValue);
+		}
+
+		public static void setLogToSTDERR(Boolean newValue)
+		{
+			setOption(new NSString("logToSTDERR"),newValue);
+		}
+
+		public static void setReinstallCrashHandlers(Boolean newValue)
+		{
+			setOption(new NSString("reinstallCrashHandlers"),newValue);
+		}
+
+		public static void setSendLogOnlyOnCrash(Boolean newValue)
+		{
+			setOption(new NSString("sendLogOnlyOnCrash"),newValue);
+		}
+
+
 
 		[DllImport ("libc")]
 		private static extern int sigaction (Signal sig, IntPtr act, IntPtr oact);
--- TestFlight/binding/testflight.cs
+++ TestFlight/binding/testflight.cs
@@ -18,10 +18,15 @@ namespace MonoTouch.TestFlight {
 		void AddCustomEnvironmentInformation (string information, string key);
 
 		[Static, Export ("takeOff:")]
-		void TakeOff (string teamToken);
+		void TakeOff (string applicationToken);
 
 		// The values that the dictionary accepts:
-		// NSString ("reinstallCrashHandlers") -> NSNumber.Boolean, set to true to reinstall the crash handlers, in case some other library does
+		// TFOptionAttachBacktraceToFeedback => NSNumber.Boolean, Defaults to @NO. Setting to @YES attaches the current backtrace, with symbols, to the feedback.
+		// TFOptionDisableInAppUpdates => NSNumber.Boolean, Defaults to @NO. Setting to @YES, disables the in app update screen shown in BETA apps when there is a new version available on TestFlight.
+		// TFOptionLogToConsole => NSNumber.Boolean, Defaults to @YES. Prints remote logs to Apple System Log.
+		// TFOptionLogToSTDERR => NSNumber.Boolean, Defaults to @YES. Sends remote logs to STDERR when debugger is attached.
+		// TFOptionReinstallCrashHandlers => NSNumber.Boolean, If set to @YES: Reinstalls crash handlers, to be used if a third party library installs crash handlers overtop of the TestFlight Crash Handlers.
+		// TFOptionSendLogOnlyOnCrash => NSNumber.Boolean, Defaults to @NO. Setting to @YES stops remote logs from being sent when sessions end. They would only be sent in the event of a crash.
 		[Static, Export ("setOptions:")]
 		void SetOptions (NSDictionary options);
 
@@ -36,5 +41,31 @@ namespace MonoTouch.TestFlight {
 
 		[Static, Export ("setDeviceIdentifier:")]
 		void SetDeviceIdentifier (string deviceIdentifer);
+
+
 	}
+
+	//The NSStrings used for the options could be exposed to C#, but this doesn't seem to work. (someone please fix)
+	//They are kept for refrence.
+	//For now these constants are not used and the bindings define them themself
+//	[Static]
+//	public interface TestFlightOptions {
+//		[Static, Field("TFOptionAttachBacktraceToFeedback","__Internal")]
+//		NSString AttachBacktraceToFeedback {get;}
+//		
+//		[Static, Field("TFOptionDisableInAppUpdates","__Internal")]
+//		NSString DisableInAppUpdates {get;}
+//		
+//		[Static, Field("TFOptionLogToConsole","__Internal")]
+//		NSString LogToConsole {get;}
+//		
+//		[Static, Field("TFOptionLogToSTDERR","__Internal")]
+//		NSString LogToSTDERR {get;}
+//		
+//		[Static, Field("TFOptionReinstallCrashHandlers","__Internal")]
+//		NSString ReinstallCrashHandlers {get;}
+//		
+//		[Static, Field("TFOptionSendLogOnlyOnCrash","__Internal")]
+//		NSString SendLogOnlyOnCrash {get;}
+//	}
 }
