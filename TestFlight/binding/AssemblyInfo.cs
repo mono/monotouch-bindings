b33e48bcec2f83c1fc62a4d52fe139d71eab05eb
Kirk Klobe <kirk_klobe@starkey.com>
2013-04-11 20:32:45 -0500

    add ArmV7s LinkTarget

--- TestFlight/binding/AssemblyInfo.cs
+++ TestFlight/binding/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libTestFlight.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true)]
+[assembly: LinkWith ("libTestFlight.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.ArmV7s, ForceLoad = true)]
