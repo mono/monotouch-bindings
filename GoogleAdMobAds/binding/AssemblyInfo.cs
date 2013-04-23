4e0bb94290d278b414f6d72920f46a14832795f7
Alex Soto <alex@alexsoto.me>
2013-04-22 23:10:47 -0500

    [GoogleAdmobAds] Removed binding directory

--- GoogleAdMobAds/binding/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System;
-using MonoTouch.ObjCRuntime;
-
-[assembly: LinkWith ("libGoogleAdMobAds.a", LinkTarget.Simulator | LinkTarget.ArmV7, ForceLoad = true, Frameworks = "AudioToolbox MessageUI SystemConfiguration CoreGraphics MediaPlayer StoreKit", WeakFrameworks = "AdSupport", IsCxx = true, LinkerFlags = "-lz -lsqlite3")]
-[assembly: LinkWith ("libAdmobExporter.a", LinkTarget.Simulator | LinkTarget.ArmV7, ForceLoad = true)]
