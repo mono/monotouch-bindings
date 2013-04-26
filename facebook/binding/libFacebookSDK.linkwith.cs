bbd29f10258755052dc29008b12a7f9490c6704c
Alex Soto <alex@alexsoto.me>
2013-04-25 21:20:14 -0500

    [Facebook] added missing Security framework

--- facebook/binding/libFacebookSDK.linkwith.cs
+++ facebook/binding/libFacebookSDK.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libFacebookSDK.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, "-ObjC -lsqlite3", ForceLoad = true, Frameworks = "CoreGraphics CoreLocation QuartzCore", WeakFrameworks = "Accounts AdSupport Social")]
+[assembly: LinkWith ("libFacebookSDK.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, "-ObjC -lsqlite3", ForceLoad = true, Frameworks = "CoreGraphics CoreLocation QuartzCore Security", WeakFrameworks = "Accounts AdSupport Social")]
