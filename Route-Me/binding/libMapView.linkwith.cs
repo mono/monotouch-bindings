b048fdcc3ec2ccc631ae0ee71418c5ada425ba1a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-05-20 13:53:00 +0200

    [RouteMe] Don't explicitly link with CoreFoundation. Fixes #12276.
    Apple's linker doesn't like it, and ends up crashing.

--- Route-Me/binding/libMapView.linkwith.cs
+++ Route-Me/binding/libMapView.linkwith.cs
@@ -2,5 +2,5 @@ using System;
 using MonoTouch.ObjCRuntime;
 
 [assembly: LinkWith ("libMapView.a", LinkTarget.ArmV7 | LinkTarget.Simulator, 
-                     Frameworks = "CoreFoundation CoreLocation QuartzCore UIKit Foundation CoreGraphics", 
+                     Frameworks = "CoreLocation QuartzCore UIKit Foundation CoreGraphics", 
                      LinkerFlags = "-lz -lsqlite3", ForceLoad = true, IsCxx = true)]
