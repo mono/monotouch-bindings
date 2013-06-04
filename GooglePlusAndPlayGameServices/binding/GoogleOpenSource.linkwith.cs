2f666ddf49c54c3d5bca53c53a5e6b543f685e73
Alex Soto <alex@alexsoto.me>
2013-06-04 00:28:59 -0500

    [GooglePlusPlay] Fixed tipo

--- GooglePlusAndPlayGameServices/binding/GoogleOpenSource.linkwith.cs
+++ GooglePlusAndPlayGameServices/binding/GoogleOpenSource.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("GoogleOpenSource.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, IsCxx = true)]
+[assembly: LinkWith ("GoogleOpenSource", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, IsCxx = true)]
