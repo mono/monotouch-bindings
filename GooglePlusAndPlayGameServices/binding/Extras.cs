5c02e44bb0705e0e6040118eb26066c155e58058
Alex Soto <alex@alexsoto.me>
2013-06-04 01:30:24 -0500

    [GooglePlusAndPlay] Added needed constants from Google OpenSource SDK, file GTLPlusConstants.h

--- GooglePlusAndPlayGameServices/binding/Extras.cs
+++ GooglePlusAndPlayGameServices/binding/Extras.cs
@@ -6,5 +6,30 @@ namespace Google.Plus
 	{
 		ShareBuilder ShareDialog { get { return new ShareBuilder (ShareDialog_); } }
 	}
+
+	public static class PlusConstants
+	{
+		// Authorization scope
+		public const string AuthScopePlusLogin = @"https://www.googleapis.com/auth/plus.login";
+		public const string AuthScopePlusMe    = @"https://www.googleapis.com/auth/plus.me";
+
+		// Collection
+		public const string PlusCollectionPlusoners = @"plusoners";
+		public const string PlusCollectionPublic    = @"public";
+		public const string PlusCollectionResharers = @"resharers";
+		public const string PlusCollectionVault     = @"vault";
+		public const string PlusCollectionVisible   = @"visible";
+
+		// OrderBy
+		public const string PlusOrderByAlphabetical = @"alphabetical";
+		public const string PlusOrderByBest         = @"best";
+		public const string PlusOrderByRecent       = @"recent";
+
+		// SortOrder
+		public const string PlusSortOrderAscending  = @"ascending";
+		public const string PlusSortOrderDescending = @"descending"; 
+		
+	}
+
 }
 
