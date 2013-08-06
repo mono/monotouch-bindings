25f57f7ca80a97611f558d531d59a933019279f2
Alex Soto <alex@alexsoto.me>
2013-08-06 17:22:44 -0400

    [SDWebImage] Made public methods on extras

--- SDWebImage/binding/Extras.cs
+++ SDWebImage/binding/Extras.cs
@@ -5,7 +5,7 @@ namespace SDWebImage
 {
 	public partial class SDWebImageDownloader
 	{
-		SDWebImageOperation DownloadImageWithURL (NSUrl url, SDWebImageDownloaderOptions options, SDWebImageDownloaderProgressHandler progressHandler, SDWebImageDownloaderCompleteHandler completedHandler)
+		public SDWebImageOperation DownloadImageWithURL (NSUrl url, SDWebImageDownloaderOptions options, SDWebImageDownloaderProgressHandler progressHandler, SDWebImageDownloaderCompleteHandler completedHandler)
 		{
 			return new SDWebImageOperation (DownloadImageWithURL_ (url, options, progressHandler, completedHandler));
 		}
@@ -13,7 +13,7 @@ namespace SDWebImage
 
 	public partial class SDWebImageManager
 	{
-		SDWebImageOperation DownloadImageWithURL (NSUrl url, SDWebImageOptions options, SDWebImageDownloaderProgressHandler progressHandler, SDWebImageCompletedWithFinishedHandler completedHandler)
+		public SDWebImageOperation DownloadImageWithURL (NSUrl url, SDWebImageOptions options, SDWebImageDownloaderProgressHandler progressHandler, SDWebImageCompletedWithFinishedHandler completedHandler)
 		{
 			return new SDWebImageOperation (DownloadWithURL_ (url, options, progressHandler, completedHandler));
 		}
