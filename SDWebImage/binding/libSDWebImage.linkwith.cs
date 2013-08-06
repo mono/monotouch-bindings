155d8d56c5f62794c76e24fb8e006b80a19a8346
Alex Soto <alex@alexsoto.me>
2013-08-06 16:53:10 -0400

    [SDWebImage] Included missing reference to ImageIO Framework

--- SDWebImage/binding/libSDWebImage.linkwith.cs
+++ SDWebImage/binding/libSDWebImage.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libSDWebImage.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, Frameworks = "CoreGraphics")]
+[assembly: LinkWith ("libSDWebImage.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, Frameworks = "CoreGraphics ImageIO")]
