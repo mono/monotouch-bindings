cff720ff97995119587aa29642dfe25afcc7abe7
Mark Brady <markdurbinbrady@gmail.com>
2013-08-24 22:32:52 -0700

    Added Flurry support for ArmV7s.

--- FlurryAnalytics/binding/AssemblyInfo.cs
+++ FlurryAnalytics/binding/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libFlurry.a", LinkTarget.Simulator | LinkTarget.ArmV7,Frameworks = "SystemConfiguration Security", ForceLoad = true)]
+[assembly: LinkWith ("libFlurry.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s,Frameworks = "SystemConfiguration Security", ForceLoad = true)]
