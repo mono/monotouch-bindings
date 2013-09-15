ed9df339f9fb379477a71c8d144cc52cdc5905ac
Alex Soto <alex@alexsoto.me>
2013-09-15 12:27:09 -0500

    [TestFlight] Added libarclite to Assemblyinfo.cs

--- TestFlight/binding/AssemblyInfo.cs
+++ TestFlight/binding/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System;
 using MonoTouch.ObjCRuntime;
 
 [assembly: LinkWith ("libTestFlight.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.ArmV7s, ForceLoad = true)]
+[assembly: LinkWith ("libarclite.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true)]
