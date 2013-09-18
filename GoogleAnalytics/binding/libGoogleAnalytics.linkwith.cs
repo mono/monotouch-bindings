48ad721d3c3be0ba00df06c60e4c3a3239748e13
Dennis Lee <technohead@gmail.com>
2013-09-17 22:15:44 -0700

    Added missing libGoogleAnalytics.linkwith.cs

---
+++ GoogleAnalytics/binding/libGoogleAnalytics.linkwith.cs
@@ -0,0 +1,5 @@
+using System;
+using MonoTouch.ObjCRuntime;
+
+//[assembly: LinkWith ("libGoogleAnalytics.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true)]
+[assembly: LinkWith ("libGoogleAnalytics.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s, ForceLoad = true)]
