4091d8f0a87e7513d2ddc9506b6bd6a39edbbb51
Alex Soto <alex@alexsoto.me>
2013-09-18 02:05:08 -0500

    [TimesSquare] Updated to latest version and added support for iOS 5

--- TimesSquare/binding/AssemblyInfo.cs
+++ TimesSquare/binding/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System;
 using MonoTouch.ObjCRuntime;
 
 [assembly: LinkWith ("libTimesSquare.a", LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, Frameworks = "CoreGraphics")]
+[assembly: LinkWith ("libarclite.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true)]
