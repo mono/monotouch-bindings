ec9924dc69ee0653ccc5729ffb1f39644a4bb897
Alex Soto <alex@alexsoto.me>
2013-09-17 22:21:49 -0500

    [SDSegmentedControl] Updated to version 2.0

--- SDSegmentedControl/binding/ApiDefinition.cs
+++ SDSegmentedControl/binding/ApiDefinition.cs
@@ -10,11 +10,14 @@ namespace SegmentedControl
 	[BaseType (typeof (UISegmentedControl))]
 	interface SDSegmentedControl {
 
-		[Export ("backgroundColor")] [New]
+		[Export ("backgroundColor", ArgumentSemantic.Retain)] [New]
 		UIColor BackgroundColor { get; set; }
 
-		[Export ("borderColor")]
+		[Export ("borderColor", ArgumentSemantic.Retain)]
 		UIColor BorderColor { get; set; }
+
+		[Export ("borderWidth", ArgumentSemantic.Assign)]
+		float BorderWidth { get; set; }
 
 		[Export ("arrowSize", ArgumentSemantic.Assign)]
 		float ArrowSize { get; set; }
@@ -25,8 +28,23 @@ namespace SegmentedControl
 		[Export ("animationDuration", ArgumentSemantic.Assign)]
 		double AnimationDuration { get; set; }
 
+		[Export ("interItemSpace", ArgumentSemantic.Assign)]
+		float InterItemSpace { get; set; }
+
 		[Export ("stainEdgeInsets", ArgumentSemantic.Assign)]
 		UIEdgeInsets StainEdgeInsets { get; set; }
+
+		[Export ("shadowColor", ArgumentSemantic.Retain)]
+		UIColor ShadowColor { get; set; }
+
+		[Export ("shadowRadius", ArgumentSemantic.Assign)]
+		float ShadowRadius { get; set; }
+
+		[Export ("shadowOpacity", ArgumentSemantic.Assign)]
+		float ShadowOpacity { get; set; }
+
+		[Export ("shadowOffset", ArgumentSemantic.Assign)]
+		SizeF ShadowOffset { get; set; }
 
 		[Export ("scrollView")]
 		UIScrollView ScrollView { get; set; }
@@ -38,10 +56,21 @@ namespace SegmentedControl
 		[Export ("imageSize", ArgumentSemantic.Assign)]
 		SizeF ImageSize { get; set; }
 
+		[Export ("titleFont", ArgumentSemantic.Retain)]
+		UIFont TitleFont { get; set; }
+
+		[Export ("selectedTitleFont", ArgumentSemantic.Retain)]
+		UIFont SelectedTitleFont { get; set; }
+
+		[Export ("titleShadowOffset", ArgumentSemantic.Assign)] [New]
+		SizeF TitleShadowOffset { get; set; }
 	}
 
 	[BaseType (typeof (UIView))]
 	interface SDStainView {
+
+		[Export ("backgroundColor", ArgumentSemantic.Retain)] [New]
+		UIColor BackgroundColor { get; set; }
 
 		[Export ("cornerRadius", ArgumentSemantic.Assign)]
 		float CornerRadius { get; set; }
