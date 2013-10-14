f904236357298056040651d75ff7a23d93688c6d
Miguel de Icaza <miguel@gnome.org>
2013-10-14 15:44:59 -0400

    Update binding

--- OpenTok/binding/opentok/StructsAndEnums.cs
+++ OpenTok/binding/opentok/StructsAndEnums.cs
@@ -4,25 +4,35 @@ namespace opentok
 {
 
 	public enum OTSessionErrorCode {
-	    AuthorizationFailure,       /* An invalid API key or token was provided */
-	    InvalidSessionId,           /* An invalid session ID was provided */
-	    ConnectionFailed,           /* There was an error connecting to OpenTok services */
-	    NoMessagingServer,          /* No messaging server is available for this session */
-	    SDKUpdateRequired           /* A new version of the OpenTok SDK is available and required to connect to this session */
+		AuthorizationFailure,
+		InvalidSessionId,
+		ConnectionFailed,
+		NoMessagingServer,
+		SDKUpdateRequired,
+		ConnectionRefused,
+		SessionStateFailed,
+		P2SessionUnsupported,
+		UnknownServerError,
+		P2SessionRequired,
+		P2SessionMaxParticipants,
+		SessionConnectionTimeout,
+		SessionCompatibilityMismatch,
+		SessionSignalConnection
 	}
 	
 	public enum OTPublisherErrorCode {
-	    NoMediaPublished,           /* Attempting to publish a stream with no audio or video */
-	    UserDeniedCameraAccess,     /* The user denied access to the camera during publishing */
-	    AlreadyPublishing,          /* Already publishing */
-	    SessionDisconnected         /* Attempting to publish to a disconnected session */
+	    NoMediaPublished,
+	    UserDeniedCameraAccess,
+	    SessionDisconnected
 	} 
 	
 	public enum OTSubscriberErrorCode {
-	    FailedToConnect,            /* Subscriber failed to connect to stream. Can reattempt connection */
-	    ConnectionTimedOut,         /* Subscriber timed out while attempting to connect to stream. Can reattempt connection */
-	    NoStreamMedia,              /* The stream has no audio or video to subscribe to  */
-	    InitializationFailure       /* Subscriber failed to initialize */
+	    FailedToConnect,
+	    ConnectionTimedOut,
+	    NoStreamMedia,
+	    InitializationFailure,
+	    InvalidStreamType,
+	    SelfSubcribeFailure
 	} 
 
 	public enum OTSessionEnvironment {
