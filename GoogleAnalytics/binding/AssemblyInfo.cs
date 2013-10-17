c877f6779ec7d8c51a589f6320ac924eaad66ea2
Mike Dussault <mike-nospam@sudolabs.com>
2013-10-16 22:43:29 -0700

    updated to latest Google Analytics (3.0)

--- GoogleAnalytics/binding/AssemblyInfo.cs
+++ GoogleAnalytics/binding/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libGoogleAnalytics.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, Frameworks = "SystemConfiguration CFNetwork CoreData", ForceLoad = true)]
+[assembly: LinkWith ("libGoogleAnalyticsServices.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, Frameworks = "SystemConfiguration CFNetwork CoreData", ForceLoad = true)]
