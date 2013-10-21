9a463f4ee5e247fce800dffc541932d00b913dc1
Alex Soto <alex@alexsoto.me>
2013-10-20 23:43:07 -0500

    [CorePlot] Update bindings to version 1.4

--- CorePlot/binding/AssemblyInfo.cs
+++ CorePlot/binding/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libCorePlot-CocoaTouch.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, Frameworks = "CoreGraphics QuartzCore", ForceLoad = true, IsCxx = true)]
+[assembly: LinkWith ("libCorePlot-CocoaTouch.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, Frameworks = "CoreGraphics QuartzCore", ForceLoad = true)]
