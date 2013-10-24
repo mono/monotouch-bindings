1532fd782a41f9c980b51e30028f6cd2c5bca387
bholmes <bill.holems@xamarin.com>
2013-10-24 15:43:36 -0400

    Fixing some formatting for an easier read.

--- iRate/binding/AssemblyInfo.cs
+++ iRate/binding/AssemblyInfo.cs
@@ -2,5 +2,9 @@ using System;
 using MonoTouch.ObjCRuntime;
 using MonoTouch.Foundation;
 
-[assembly:LinkerSafe]
-[assembly: LinkWith ("libiRate.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, ForceLoad = true, Frameworks = "StoreKit", SmartLink=true)]
+[assembly: LinkerSafe]
+[assembly: LinkWith ("libiRate.a", 
+                     LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, 
+                     ForceLoad = true, 
+                     Frameworks = "StoreKit", 
+                     SmartLink=true)]
