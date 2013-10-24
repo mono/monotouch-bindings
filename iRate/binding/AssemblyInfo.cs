76a76e5a6cc3cf31b018d04af2cc67166aef50b1
bholmes <bill.holems@xamarin.com>
2013-10-24 15:32:37 -0400

    Adding linker optimizations to the iRate component.
    Adding LinkerSafe and SmartLink.

--- iRate/binding/AssemblyInfo.cs
+++ iRate/binding/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
 
-[assembly: LinkWith ("libiRate.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, ForceLoad = true, Frameworks = "StoreKit")]
+[assembly:LinkerSafe]
+[assembly: LinkWith ("libiRate.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, ForceLoad = true, Frameworks = "StoreKit", SmartLink=true)]
