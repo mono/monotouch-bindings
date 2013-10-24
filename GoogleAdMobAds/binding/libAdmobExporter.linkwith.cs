f7d2b88f14d615e48425b8b9747292d160eb7b15
Alex Soto <alex@alexsoto.me>
2013-10-24 11:24:02 -0500

    [GoogleAdMob] Provided an armv7s version of the libAdmobExporter.a, and some bugfixes in the binding

--- GoogleAdMobAds/binding/libAdmobExporter.linkwith.cs
+++ GoogleAdMobAds/binding/libAdmobExporter.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libAdmobExporter.a", LinkTarget.Simulator | LinkTarget.ArmV7, ForceLoad = true)]
+[assembly: LinkWith ("libAdmobExporter.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, ForceLoad = true)]
