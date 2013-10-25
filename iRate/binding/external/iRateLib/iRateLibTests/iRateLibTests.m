e9b53dbcedb3dbc9511a886dde9e72c4736b6e94
Alex Soto <alex@alexsoto.me>
2013-10-25 13:31:53 -0500

    Revert "[iRate] Removed some files and updated the clean target"
    This reverts commit ffb1a9b5ed2768c51ab30640992f9c3432866a9b.

---
+++ iRate/binding/external/iRateLib/iRateLibTests/iRateLibTests.m
@@ -0,0 +1,34 @@
+//
+//  iRateLibTests.m
+//  iRateLibTests
+//
+//  Created by Bill Holmes on 10/18/13.
+//  Copyright (c) 2013 Xamarin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iRateLibTests : XCTestCase
+
+@end
+
+@implementation iRateLibTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
