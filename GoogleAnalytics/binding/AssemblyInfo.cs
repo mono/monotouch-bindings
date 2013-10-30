f06eb84b185f7151baca7cc279f7a19b955f7aaf
Alex Soto <alex@alexsoto.me>
2013-10-30 13:34:10 -0600

    [GoogleAnalytics] Several fixes and additions NO API CHANGES still on 2.0beta 	* Added binding project 	* Fixed binding/Makefile to download the right version of the library 	* renamed sample to samples folder 	* Added sample builder Makefile

--- GoogleAnalytics/binding/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using MonoTouch.ObjCRuntime;
-
-[assembly: LinkWith ("libGoogleAnalytics.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, Frameworks = "SystemConfiguration CFNetwork CoreData", ForceLoad = true)]
