f7432954a918011a3bfb7af95b365541b58fa7ff
Alex Soto <alex@alexsoto.me>
2013-11-05 01:21:01 -0600

    [ShareKit] added xcode source v0.2.1 and added ShareKit License.md

---
+++ ShareKit/binding/ShareKitLibrary/ShareKitLibraryTests/ShareKitLibraryTests.m
@@ -0,0 +1,34 @@
+//
+//  ShareKitLibraryTests.m
+//  ShareKitLibraryTests
+//
+//  Created by Alex Soto on 11/5/13.
+//  Copyright (c) 2013 Alex Soto. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ShareKitLibraryTests : XCTestCase
+
+@end
+
+@implementation ShareKitLibraryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
