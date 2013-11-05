b1b936a2442bf8bcb4485a6a395d3c7255b54bb8
Alex Soto <alex@alexsoto.me>
2013-11-05 01:41:38 -0600

    [TapIt] added missing frameworks, added sample builder script, added samples folder

--- Tapit/Binding/libTapIt.linkwith.cs
+++ Tapit/Binding/libTapIt.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libTapIt.a", LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true)]
+[assembly: LinkWith ("libTapIt.a", LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, Frameworks = "SystemConfiguration CoreGraphics QuartzCore CoreTelephony")]
