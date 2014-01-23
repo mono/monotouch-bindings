5b22e6e78032e75d03246c327a679bc6cd5f8dfa
Alex Soto <alex@alexsoto.me>
2014-01-23 01:04:46 -0600

    [GoogleAdmobAds] Added missing framework in version 6.7.0

--- GoogleAdMobAds/binding/libGoogleAdMobAds.linkwith.cs
+++ GoogleAdMobAds/binding/libGoogleAdMobAds.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libGoogleAdMobAds.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, SmartLink = true, ForceLoad = true, Frameworks = "AudioToolbox MessageUI SystemConfiguration CoreGraphics MediaPlayer StoreKit", WeakFrameworks = "AdSupport", LinkerFlags = "-lz -lsqlite3 -ObjC")]
+[assembly: LinkWith ("libGoogleAdMobAds.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, SmartLink = true, ForceLoad = true, Frameworks = "AudioToolbox MessageUI SystemConfiguration CoreGraphics MediaPlayer StoreKit AVFoundation AdSupport", LinkerFlags = "-lz -lsqlite3 -ObjC")]
