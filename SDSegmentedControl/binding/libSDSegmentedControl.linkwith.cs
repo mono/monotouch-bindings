c82dbde896b3f8ea96700b4e034f6d5d098fcd2c
Alex Soto <alex@alexsoto.me>
2014-03-20 07:14:02 -0600

    [SDSegmentedControl] Get rid of arclite

--- SDSegmentedControl/binding/libSDSegmentedControl.linkwith.cs
+++ SDSegmentedControl/binding/libSDSegmentedControl.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libSDSegmentedControl.a", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.ArmV7s, Frameworks = "QuartzCore CoreGraphics", ForceLoad = true)]
+[assembly: LinkWith ("libSDSegmentedControl.a", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.ArmV7s, LinkerFlags = "-ObjC -fobjc-arc", Frameworks = "QuartzCore CoreGraphics", ForceLoad = true)]
