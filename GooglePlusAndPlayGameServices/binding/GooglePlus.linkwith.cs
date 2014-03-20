826068dc79427857816dc3d67abbc09fa4c34a4e
Alex Soto <alex@alexsoto.me>
2014-03-20 00:09:50 -0600

    [GooglePlusAndPlay] Remove libarclite dependency

--- GooglePlusAndPlayGameServices/binding/GooglePlus.linkwith.cs
+++ GooglePlusAndPlayGameServices/binding/GooglePlus.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("GooglePlus", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, Frameworks = "SystemConfiguration Security", ForceLoad = true)]
+[assembly: LinkWith ("GooglePlus", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, LinkerFlags = "-ObjC -fobjc-arc", Frameworks = "SystemConfiguration Security", ForceLoad = true)]
--- GooglePlusAndPlayGameServices/binding/PlayGameServices.linkwith.cs
+++ GooglePlusAndPlayGameServices/binding/PlayGameServices.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("PlayGameServices", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, Frameworks = "CoreData Security SystemConfiguration", ForceLoad = true)]
+[assembly: LinkWith ("PlayGameServices", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, LinkerFlags = "-ObjC -fobjc-arc", Frameworks = "CoreData Security SystemConfiguration", ForceLoad = true)]
