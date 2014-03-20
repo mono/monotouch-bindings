cc207fddd9de32ee9d39effcbf0c3a6913f976b7
Alex Soto <alex@alexsoto.me>
2014-03-20 00:09:50 -0600

    [GooglePlusAndPlay] Remove libarclite dependency

--- GooglePlusAndPlayGameServices/binding/GoogleOpenSource.linkwith.cs
+++ GooglePlusAndPlayGameServices/binding/GoogleOpenSource.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("GoogleOpenSource", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, IsCxx = true)]
+[assembly: LinkWith ("GoogleOpenSource", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, LinkerFlags = "-ObjC -fobjc-arc", ForceLoad = true, IsCxx = true)]
