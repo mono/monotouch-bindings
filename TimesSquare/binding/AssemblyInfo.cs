716f2612e30e6c03510eced3485c2a5b2a7d3c54
Alex Soto <alex@alexsoto.me>
2014-04-09 12:37:26 -0500

    [TimesSquare] Updated to latest available version in master branch 	* commit bound: https://github.com/square/objc-TimesSquare/commit/5e394b17eae2016e9f3f545e7945a382f3811e8c

--- TimesSquare/binding/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using MonoTouch.ObjCRuntime;
-
-[assembly: LinkWith ("libTimesSquare.a", LinkTarget.ArmV7 | LinkTarget.Simulator, LinkerFlags = "-ObjC -fobjc-arc", ForceLoad = true, Frameworks = "CoreGraphics")]
