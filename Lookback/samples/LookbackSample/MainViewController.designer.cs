9f4d983e1711c2f6d76dcec45ec4fc9af10d7b9e
Chris Stewart <socialtopher@gmail.com>
2014-08-20 23:03:10 -0500

    Add Lookback SDK support
    Adds binding for [Lookback](http://lookback.io), which allows users to record experiences in your application and upload them for your review. Whether used for bug reports, feature requests, or general communication, this tool is fun and seriously useful.

---
+++ Lookback/samples/LookbackSample/MainViewController.designer.cs
@@ -0,0 +1,24 @@
+﻿// WARNING
+//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
+
+namespace LookbackSample
+{
+	[Register ("MainViewController")]
+	partial class MainViewController
+	{
+		[Action ("showInfo:")]
+		[GeneratedCodeAttribute ("iOS Designer", "1.0")]
+		partial void showInfo (MonoTouch.Foundation.NSObject sender);
+
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
+
