f1b0b0999e44a0627dfc522a4a8418899d3cb5a0
gilm <gil@megidish.net>
2015-01-25 14:01:15 +0200

    [TestFairy] Added AssemblyInfo.cs file

---
+++ TestFairy/binding/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+[assembly: AssemblyTitle("TestFairy.iOS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright (c) 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
